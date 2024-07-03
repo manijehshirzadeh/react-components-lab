aa1acc77c327926f73f2752ff23158d0f4a8bb0d
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-07-03 16:47:39 +1000

    Create the WeatherForecast.jsx and WeatherForecast.css. Add some starter code

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,20 @@
+import "./weatherForecasts.css";
+
+const weatherForecasts = () => {
+  return (
+    <>
+      <div className="weather">
+        <h2>Day of the Week</h2>
+        <img src="" alt="" />
+        <p>
+          <span>conditions: </span>current weather conditions
+        </p>
+        <p>
+          <span>time: </span>time of day
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default weatherForecasts;
